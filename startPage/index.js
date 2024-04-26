e85ccae34962d486284fc8226bbd8b70eb82b482
itsDipsyProjects <elliot.collins.git@gmail.com>
2024-04-26 16:17:10 +0200

    Uppdate

--- startPage/index.js
+++ startPage/index.js
@@ -30,10 +30,43 @@ async function who_do_you_know(names) {
                 let request = await fetch("http://localhost:4000/?test=hej");
                 let data = await request.text();
                 console.log(data);
+                let intro_text_dom1 = document.querySelector("#text1");  
+                console.log(intro_text_dom1);
+                let intro_text_dom2 = document.querySelector("#text2");  
 
-                if(data === "Allowed"){
-                    window.location.replace("http://localhost:4000/firstStation/index.html");
-                }
+                let intro_text_first = `Hej och välkomna till Malmö Underground du kommer nu få instruktioner så lyssna noga! `; 
+                let intro_text_first2 = `Du kommer få hänga med på en utekväll som du efter några enheter lär glömma så sätt på cassetbandet och lyssna`; 
+
+                let arrayOfLettersInText1 = intro_text_first.split("")
+                console.log(arrayOfLettersInText1);
+                let arrayOfLettersInText2 = intro_text_first2.split("")
+
+                let i = 0;
+                let wait_before_intro_is_done = false;
+                let j = 0;
+                
+
+                let intervalId1 = setInterval(() => {
+                    if (i < arrayOfLettersInText1.length) {
+                        intro_text_dom1.innerHTML += arrayOfLettersInText1[i];
+                        i++;
+                    } else {
+                        clearInterval(intervalId1);
+                        let intervalId2 = setInterval(() =>{
+                            if (j < arrayOfLettersInText2.length) {
+                                intro_text_dom2.innerHTML += arrayOfLettersInText2[j];
+                                j++;
+                            } else {
+                                clearInterval(intervalId2);
+                                wait_before_intro_is_done = true;   
+                                if(data === "Allowed" && wait_before_intro_is_done === true){
+                                    window.location.replace("http://localhost:4000/firstStation/index.html");
+                                }
+                            }
+                        },100)
+                    }
+                }, 100);                   
+                
             }
             the_input.value = "";
             wrongOrRight = 0;

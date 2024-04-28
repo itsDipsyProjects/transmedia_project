3c1ef315bd1458201c9697e21cc8eea2501a1105
itsDipsy <elliot.collinszcv@gmail.com>
2024-04-28 22:27:29 +0200

    big uppdate

--- firstStation/canvas.js
+++ firstStation/canvas.js
@@ -1,88 +1,115 @@
+function initCanvas(){
+    const canvas = document.querySelector('canvas');
 
-const canvas = document.querySelector('canvas');
-
-// Get the 2D rendering context
-const ctx = canvas.getContext('2d');
-
-let canvasWidth = 355, canvasHeight = 355;
-
-canvas.width = canvasWidth;
-canvas.height = canvasHeight;
-
-
-
-const img1 = new Image();
-
-img1.src = "../media/notClicked.png";
-
-
-const img2 = new Image();
-
-img2.src = "../media/clicked.png";
-
-
-const img3 = new Image();
-
-img3.src = "../media/stopClicked.png";
-
-const rectX = 93;
-const rectY = 268;
-const rectWidth = 50;
-const rectHeight = 70;
-
-
-const rectX2 = 153;
-const rectY2 = 268;
-const rectWidth2 = 50;
-const rectHeight2 = 70;
-
-
-let playClickCounter = 0;
-
-img1.onload = function() {
+    // Get the 2D rendering context
+    const ctx = canvas.getContext('2d');
     
-    ctx.drawImage(img1, -20, -220); 
+    let canvasWidth = 355, canvasHeight = 355;
     
-    ctx.fillStyle = 'rgba(0, 0, 0,0)'; 
-    ctx.strokeStyle = 'rgba(10, 200, 00)'; 
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
     
-    ctx.strokeRect(rectX, rectY, rectWidth, rectHeight); 
+    
+    
+    const img1 = new Image();
+    
+    img1.src = "../media/notClicked.png";
+    
+    
+    const img2 = new Image();
+    
+    img2.src = "../media/clicked.png";
+    
+    
+    const img3 = new Image();
+    
+    img3.src = "../media/stopClicked.png";
+    
+    const rectX = 93;
+    const rectY = 268;
+    const rectWidth = 50;
+    const rectHeight = 70;
+    
+    
+    const rectX2 = 153;
+    const rectY2 = 268;
+    const rectWidth2 = 50;
+    const rectHeight2 = 70;
+    
+    
 
-
-    ctx.fillStyle = 'rgba(0, 0, 0,0)'; 
-    ctx.strokeStyle = 'rgba(10, 0, 200)'; 
-    
-    ctx.strokeRect(rectX2, rectY2, rectWidth2, rectHeight2); 
-
-    
-    canvas.addEventListener('click', function(event) {
-        // Get the click coordinates relative to the canvas
-        const x = event.offsetX;
-        const y = event.offsetY;
+    img1.onload = function() {
         
-        // Check if the click occurred within the rectangle bounds
-        if (x >= rectX && x <= rectX + rectWidth && y >= rectY && y <= rectY + rectHeight && playClickCounter === 0) {
-            ctx.clearRect(0,0,canvasWidth, canvasHeight)
-            ctx.drawImage(img2,-20,-220)
+        if(playClickCounter === 0){
+            ctx.drawImage(img1, -20, -220); 
             
-            const audio = new Audio();
+            ctx.fillStyle = 'rgba(0, 0, 0,0)'; 
+            ctx.strokeStyle = 'rgba(10, 200, 00)'; 
             
-            audio.src = "../media/test.wav"; 
-            
-            audio.play();
-            playClickCounter = 1;
+            ctx.strokeRect(rectX, rectY, rectWidth, rectHeight); 
         }
 
-        if (x >= rectX2 && x <= rectX2 + rectWidth2 && y >= rectY2 && y <= rectY2 + rectHeight2 && playClickCounter === 1) {
-            ctx.clearRect(0,0,canvasWidth, canvasHeight)
-            ctx.drawImage(img3,-20,-220)
+
+        if(playClickCounter === 2){
+            ctx.drawImage(img2, -20, -220); 
             
-            const audio = new Audio();
+            ctx.fillStyle = 'rgba(0, 0, 0,0)'; 
+            ctx.strokeStyle = 'rgba(10, 200, 00)'; 
             
-            audio.src = "../media/test.wav"; 
-            
-            audio.play();
-            playClickCounter = 0;
+            ctx.strokeRect(rectX2, rectY2, rectWidth, rectHeight); 
         }
-    });
-};
+
+        if(playClickCounter === 3){
+            ctx.drawImage(img3, -20, -220); 
+            
+            ctx.fillStyle = 'rgba(0, 0, 0,0)'; 
+            ctx.strokeStyle = 'rgba(10, 200, 00)'; 
+            
+            ctx.strokeRect(rectX, rectY, rectWidth, rectHeight); 
+        }
+
+    
+    
+      
+    
+        
+        
+    
+        canvas.addEventListener('click', function(event) {
+
+            const x = event.offsetX;
+            const y = event.offsetY;
+            
+            // Play Clicked
+            if (x >= rectX && x <= rectX + rectWidth && y >= rectY && y <= rectY + rectHeight && playClickCounter === 0 || playClickCounter === 3) {
+                ctx.clearRect(0,0,canvasWidth, canvasHeight)
+                ctx.drawImage(img2,-20,-220)
+                
+                
+                
+                audio.play();
+                
+                audio2.play();
+    
+                playClickCounter = 2;
+            }
+    
+            //Stop Clicked
+            if (x >= rectX2 && x <= rectX2 + rectWidth2 && y >= rectY2 && y <= rectY2 + rectHeight2 && playClickCounter === 2) {
+                ctx.clearRect(0,0,canvasWidth, canvasHeight)
+                ctx.drawImage(img3,-20,-220)
+                
+                
+                audio.play();
+    
+                audio2.pause();
+                playClickCounter = 3;
+            }
+        });
+    };
+}
+    
+    
+
+
+
--- firstStation/index.js
+++ firstStation/index.js
@@ -1,1 +1,41 @@
-let btn_to_bilder = document.querySelector("button");
+
+
+function load(){    
+    console.log(playClickCounter)
+    let container = document.querySelector("#container");
+    container.innerHTML = `
+    
+        <div id="firstPart">
+            <img id="title" src="../media/Group 18.png" alt="">
+            <canvas id="myCanvas" ></canvas>
+        </div>
+        <div id="secondPart">
+            <div id="container_for_cig_and_btn">
+                <div id="cigarets"></div>
+                <div id="container_for_btn">
+                    <button id="to_bilder">Bilder</button>
+                </div>
+            </div>
+        </div>
+
+
+    `;
+    initCanvas();
+
+    let btn_to_bilder = document.querySelector("#to_bilder");
+    let firstPartDom = document.querySelector("#firstPart");
+    
+    btn_to_bilder.addEventListener("click", () => {
+        firstPartDom.innerHTML = ``;
+        let back_btn = btn_to_bilder;
+        back_btn.id = "back";
+        back_btn.textContent = "back";
+
+        back_btn.addEventListener("click", () => {
+            load();
+        });
+    });
+  
+}
+
+load();

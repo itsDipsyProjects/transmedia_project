72de95a669ec3b9e04f89ffa5663f9c0693184ff
itsDipsy <elliot.collinszcv@gmail.com>
2024-05-07 14:30:20 +0200

    uppdate

--- dragAndDrop/index.js
+++ dragAndDrop/index.js
@@ -10,7 +10,30 @@ function randomizeLetters() {
     if(the_static_letters.length !== 0){
         counter++;
         randomizeLetters();
-    }        
+    }
+    else{
+        let id_of_the_dragable_dom = "";
+        let letters_inventory_dom = document.querySelectorAll("#your_letters_inventory div");
+        let secret_word_divs_dom = document.querySelectorAll("#secretWord div");
+        let dropables = document.querySelectorAll(".droppable");
+        console.log(dropables);
+        letters_inventory_dom.forEach((element, index) => {
+            element.innerHTML = `
+                ${the_real_array[index]}
+            `;
+            element.addEventListener("drag", (event) => {
+                console.log("drag");
+                id_of_the_dragable_dom = event.currentTarget.id;
+            })
+        })
+        dropables.forEach((dropableDom) => {
+            dropableDom.addEventListener("mouseover", () => {
+                console.log('drop');
+                let dragDom = document.querySelector(`#${id_of_the_dragable_dom }`)
+                dropableDom.replaceWith(dragDom)
+            })
+        })
+    }
 }
 
 

5dfb7dc929d1e67192b7ed266c10fb758deb5cce
itsDipsy <elliot.collinszcv@gmail.com>
2024-05-07 15:57:56 +0200

    drag and drop a little more tweaked
    Now it checks if all the boxes have been checked

--- dragAndDrop/index.js
+++ dragAndDrop/index.js
@@ -2,13 +2,22 @@ let the_static_letters = ["s","i","b","y","l","l","a"];
 let random_letters = ["d","e","c","f","g","m","n","o","p","y","x","å","ä","ö"];
 let counter = 0;
 let the_real_array = [];
+
+
 let dropables = document.querySelectorAll(".droppable");
 let letters_inventory_dom = document.querySelectorAll("#your_letters_inventory div");
 let secret_word_divs_dom = document.querySelectorAll("#secretWord div");
 let id_drag = ""
 
 
+let gameDone = 0;
+
 function randomizeLetters() {  
+    
+    function getRandomIndex(array) {
+        let randomIndex = Math.floor(Math.random() * (array.length)) ;    
+        return randomIndex;
+    }
 
     let randomIndex = getRandomIndex(the_static_letters)
     let test = the_static_letters.splice(randomIndex,1);
@@ -22,44 +31,61 @@ function randomizeLetters() {
             element.innerHTML = `
                 ${the_real_array[index]}
             `;  
-            element.addEventListener("dragstart", (event) => {
-                id_drag = event.target.id
-                console.log(id_drag);
-            })
-            
-            
+         
         })
-        
-        dropables.forEach((dropable) => {
-             dropable.addEventListener("dragover", function (event){
-                 console.log(id_drag);
-                 let dragableDom = document.querySelector(`#${id_drag}`)
-                 dragableDom.addEventListener("dragend", () => {
-                     dragableDom.classList.remove("draggable")
-                     dragableDom.attributes.removeNamedItem("draggable")
-                    dropable.replaceWith(dragableDom);
-                })
-            })
-           
-        })
-        
     }
+    
 }
 
-
-function getRandomIndex(array) {
-    let randomIndex = Math.floor(Math.random() * (array.length)) ;    
-    return randomIndex;
-}
 
 
 
 function start(){
     randomizeLetters();
+    dragAndDrop();
 }
 
 
-start();
 
-console.log(the_real_array);
+function dragAndDrop(){
+    letters_inventory_dom.forEach((element, index) => {
+        element.addEventListener("dragstart", (event) => {
+            id_drag = event.target.id
+            console.log(id_drag);
+        })
+    })
+    dropables.forEach((dropable) => {
+         dropable.addEventListener("dragover", function (event){
+             console.log(id_drag);
+             let dragableDom = document.querySelector(`#${id_drag}`)
+             dragableDom.addEventListener("dragend", () => {
+                dropable.replaceWith(dragableDom);
+                dragableDom.attributes.removeNamedItem("draggable")
+            })
+        })
+       
+    })
+
+}
+
+function gameLoop() {
+    // Update animation state
+    console.log("hello");
+    let check = document.querySelectorAll(".droppable");
+    if(check.length === 0){
+        prompt("Done")
+        
+    }
+    else{
+        requestAnimationFrame(gameLoop);
+    }
+    // Request next frame
+}
+  
+
+  
+  
+start();
+requestAnimationFrame(gameLoop);
+
 

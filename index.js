b5e71e7e6bd0612c3b2b54c561d53597d521e88f
itsDipsyProjects <elliot.collins.git@gmail.com>
2024-05-13 15:21:38 +0200

    test

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ async function who_do_you_know(names) {
                                 clearInterval(intervalId2);
                                 wait_before_intro_is_done = true;   
                                 if(wait_before_intro_is_done === true){
-                                    window.location.replace("./Annalindsplats/");
+                                    window.location.replace("./");
                                 }
                             }
                         },100)

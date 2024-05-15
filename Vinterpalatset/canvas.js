52fa51594b3897c98df4e49eb17bd4b5b52cc8be
itsDipsyProjects <elliot.collins.git@gmail.com>
2024-05-15 12:41:55 +0200

    Revert "uppdate fixed so that there is only one question in quiz"
    This reverts commit fd1ffb78ebf0e9b51cb1389be2620affe104607f.

--- Vinterpalatset/canvas.js
+++ Vinterpalatset/canvas.js
@@ -165,7 +165,53 @@ export function quizFunctionality(){
                     let checkedLabel = document.querySelector('label[for="' + radioBtn.id + '"]').textContent;
                     
                     if(checkedLabel === answer1 ){
-                        window.location.replaceI("./finished/index.html")                                                
+                        console.log("right");
+                        quizQuestionsAnswerd++;
+                        questionCounter++;
+                        quizFunctionality();
+                    }
+                    else{
+                        tryAgain = true
+                        quizFunctionality();
+                    }
+                }
+            })
+        })
+    }
+
+    if(tryAgain === false && quizQuestionsAnswerd === 1){
+        firstPartDom.innerHTML = `
+            <img id="title" src="../media/cat.svg" alt="">
+            <div id="windows_container">
+                <div id="top_blue_border">
+                    <div id="rubrik">Quiz</div>
+                    <div id="icon"></div>
+                </div>
+                <div id="question_container">
+                    <p>${questions[quizQuestionsAnswerd]}</p>
+                    <form>
+                        <input type="radio" name="option" id="firstQuestion" value="firstQuestion">
+                        <label for="firstQuestion">${optionsForQuiz2[0]}</label><br>
+                        <input type="radio" name="option" id="secondQuestion" value="SecondQuestion">
+                        <label for="secondQuestion">${optionsForQuiz2[1]}</label><br>
+                        <input type="radio" name="option" id="thirdQuestion" value="thirdQuestion">
+                        <label for="thirdQuestion">${optionsForQuiz2[2]}</label>
+                    </form>
+
+                    <p>Fråga ${questionCounter}/2</p>
+                </div>
+            </div>
+        `;
+
+        let radioBtns = document.getElementsByName('option');
+        radioBtns.forEach((radioBtn) => {
+            radioBtn.addEventListener("click", () => {
+                if(radioBtn.checked){
+                    let checkedLabel = document.querySelector('label[for="' + radioBtn.id + '"]').textContent;
+                    
+                    if(checkedLabel === answer2 ){
+                        console.log("right");
+                        window.location.replace("./finished/index.html")
                     }
                     else{
                         tryAgain = true
